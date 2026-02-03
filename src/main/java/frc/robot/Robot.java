081dd0677e0e13ca80f386278e8f85aa868a50ff
TheMrStealth <90276412+TheMrStealth@users.noreply.github.com>
2026-02-03 16:38:13 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,9 +8,11 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Climb;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Launcher;
+import frc.robot.subsystems.Omnispike;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
@@ -20,6 +22,8 @@ public class Robot extends TimedRobot {
   private Drive driveS;
   private Intake intakeS;
   private Launcher launcherS;
+  private Omnispike omnispikeS;
+  private Climb climbS;
   private XboxController driver, operator;
 
   public Robot() {
@@ -28,6 +32,8 @@ public class Robot extends TimedRobot {
     driveS = new Drive();
     intakeS = new Intake();
     launcherS = new Launcher();
+    omnispikeS = new Omnispike();
+    climbS = new Climb();
     //Controllers
     driver = new XboxController(0);
     operator = new XboxController(1);
