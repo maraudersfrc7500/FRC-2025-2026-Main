bdd8e6fa17ae86767069d771e0a9389c657bec12
TheMrStealth <90276412+TheMrStealth@users.noreply.github.com>
2026-03-04 15:43:43 -0500

    fixed launcher for neo motors

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -18,17 +18,20 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class Launcher extends SubsystemBase {
-    // private final SparkMax motorL9, motorL10;
-    private final TalonFX motorLL9, motorLF10;
+    private final SparkMax motorLL9, motorLF10;
 
     public Launcher() {
-        motorLL9 = new TalonFX(9);
-        motorLF10 = new TalonFX(10);
+        motorLL9 = new SparkMax(9, MotorType.kBrushless);
+        motorLF10 = new SparkMax(10, MotorType.kBrushless);
         
-        motorLL9.setNeutralMode(NeutralModeValue.Brake);
-        motorLF10.setNeutralMode(NeutralModeValue.Brake);
-
-        motorLF10.setControl(new Follower(motorLL9.getDeviceID(), MotorAlignmentValue.Aligned));
+        SparkMaxConfig configLL9 = new SparkMaxConfig();
+        configLL9.idleMode(IdleMode.kBrake);
+        SparkMaxConfig configLF10 = new SparkMaxConfig();
+        configLF10.idleMode(IdleMode.kBrake);
+        configLF10.follow(motorLL9);
+        
+        motorLL9.configure(configLL9, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        motorLF10.configure(configLF10, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     public void enable() {
