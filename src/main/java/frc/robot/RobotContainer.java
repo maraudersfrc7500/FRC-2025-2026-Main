90df486b13130029919a2329a3417413c731ab69
TheMrStealth <90276412+TheMrStealth@users.noreply.github.com>
2026-03-20 16:06:14 -0400

    Trying to fix distance

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -165,30 +165,20 @@ import edu.wpi.first.wpilibj.Alert;
 
       launcherS.spin(deadband(operator.getRightTriggerAxis()));
 
-<<<<<<< Updated upstream
-      // calculateDistance();
-      
-    }
-    // public double calculateDistance() {
-    //   private a1 = 
-    //   return 1;
-    // }
-=======
       
     }
     
     public double calculateDistance() {
-      double a1 = 0;
+      double a1 = 81.22218;
       double a2 = LimelightHelpers.getTX("limelight-front");
       double h1 = 23.25;
       double h2 = 44.25;
       double height_diff=(h2-h1);
-      double angle = Math.toRadians(Math.abs(a1+a2));
+      double angle = Math.toRadians(Math.abs(a2)+Math.abs(a1));
       double distance = (height_diff/Math.tan(angle));
       return distance;
     }
       
->>>>>>> Stashed changes
     public void telemetry() {
       SmartDashboard.putNumber("XPos: ", driveS.getX());
       SmartDashboard.putNumber("YPos: ",driveS.getY());
